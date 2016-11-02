deab60164f1d489c6d6a1022a972c26514811b0d
Shane Barringer <shane.barringer@outlook.com>
2016-11-02 08:44:32 -0400

    adds RESTful routes for tweets and implements new/create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
 
-  get 'tweets/new'
+  resources :tweets
 
-  get 'tweets/edit'
-
-  get 'tweets/show'
-
-  get 'tweets/_form'
-
+  root 'tweets#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
