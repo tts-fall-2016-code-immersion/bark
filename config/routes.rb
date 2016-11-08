7a6007114794d5ffb4566892c2aa96395b7488f5
Shane Barringer <shane.barringer@outlook.com>
2016-11-07 23:08:16 -0500

    adds relationships and likes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   get "profiles" => "profiles#index"
   get 'profiles/:id' => 'profiles#show', as: :profile
 
-  resources :tweets
+  resources :tweets do
+      resource :like
+  end
+
+  resources :relationships
 
   root 'tweets#index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
